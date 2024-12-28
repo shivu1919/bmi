78484892779f6c8d5736d38ded69981b7c5dece4
shivu1919 <shivamthakur.ss129@gmail.com>
2024-12-28 14:22:02 +0530

    ss

--- script.js
+++ script.js
@@ -10,5 +10,30 @@ function CalculateBMI(){
     let height = (feet*12 + inch ) / 39.37007874;
     
     let bmi = weight / (height*height)
-    document.getElementById('result').innerHTML="Your BMI is: "+bmi
+    let status;
+
+    if(bmi<16.5){
+        status="severly underweight"
+    }
+
+    if(bmi>=16.5 && bmi<18.5){
+        status="underweight"
+    }
+
+    if(bmi>=18.5 && bmi<25.5){
+        status="Normal"
+    }
+    
+    if(bmi>=25.5 && bmi<30.5){
+        status="OverWeight"
+    }
+
+    if(bmi>=30.5 && bmi<35.5){
+        status="Obese"
+    }
+
+    if(bmi>=35.5){
+        status="Severly Obese"
+    }
+    document.getElementById('result').innerHTML="Your BMI is: "+bmi +" "+ "You are: "+ status
 }
